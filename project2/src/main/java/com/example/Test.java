2e299739ec94324719c655361245cfedfc670b8e
ahmed omar <ahmedomar1997.aoo@gmail.com>
2026-07-08 12:20:52 +0300

    handle project

---
+++ project2/src/main/java/com/example/Test.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.example.calculator.Calculator;
+
+public class Test {
+
+public static void main(String[] args) {
+
+    int addResult = Calculator.add(1, 2);
+
+    System.out.println(addResult);
+}
+
+
+
+}
